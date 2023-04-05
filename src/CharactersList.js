4859c670601d1922102c1557103a352d81c09f98
R-Pavi <pavithra@besquare.com.my>
2023-04-05 14:08:35 +0800

    Added search bar layout

---
+++ src/CharactersList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchCharacter from './SearchCharacter';
+
+
+function CharacterList(){
+    return(
+
+        <div>
+            <h1>Characters</h1>
+            <SearchCharacter/>
+        </div>
+
+    );
+}
+
+export default CharacterList;
