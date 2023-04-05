472ba11b166087e0a727369c7249071dbeecc8e9
r00khaCk <deilanraj9799@gmail.com>
2023-04-06 02:09:22 +0800

    Added the images and description for HeroDetails.jsx. Did some slight css changes in marvel.scss file.

--- src/HeroDetails.jsx
+++ src/HeroDetails.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import hulk from "./Images/imgbin-hulk-L9NMbmufEtNV6JjTcX5rRPpvg-removebg-preview.png";
+import axios from "axios";
 
 const HeroDetails = () => {
-  return <div>HeroDetails</div>;
+  const [url, setUrl] = useState(
+    "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=ecb5b76db70043b36c65f8dc830aeab1&hash=65d325a029afb4ac68f2a2d5ce99ce21"
+  );
+  const [heroItem, setHeroItem] = useState();
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(url);
+      console.log(response.data.data.results);
+      setHeroItem(response.data.data.results);
+    };
+    fetch();
+  }, [url]);
+
+  return (
+    <>
+      {/* ISSUE: Uncaught TypeError: Cannot read properties of undefined (reading 'map') */}
+      {heroItem?.map((item) => {
+        return (
+          <div className="hero-details">
+            {/* ISSUE: Warning: Each child in a list should have a unique "key" prop.*/}
+            <div className="hero-img">
+              <img
+                src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                alt=""
+                key={item.id}
+              />
+            </div>
+
+            <div className="hero-information">
+              <h1 className="hero-information-name" key={item.id}>
+                {item.name}
+              </h1>
+              <div>
+                <h3 className="hero-information-description" key={item.id}>
+                  {item.description}
+                </h3>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default HeroDetails;
