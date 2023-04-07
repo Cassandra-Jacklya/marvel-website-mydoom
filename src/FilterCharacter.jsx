32fbcef8d3e2b262a69c428480aa4e143d9fa984
Cassandra-Jacklya <cassandrajacklya7@gmail.com>
2023-04-07 12:01:03 +0800

    avengers pictures with swipe feature added

--- src/FilterCharacter.jsx
+++ src/FilterCharacter.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 function FilterCharacter({characters}) {
-  //filter the one with less comics than 25
-  const lessPopular = characters.filter((character) => character.comics.available < 25);
-  const popular = characters.filter((character) => character.comics.available >= 25);
+  // //filter the one with less comics than 25
+  // const lessPopular = characters.filter((character) => character.comics.available < 25);
+  // const popular = characters.filter((character) => character.comics.available >= 25);
 
 
-   return { lessPopular, popular }(
-     <div className="filter-container">
+  //  return { lessPopular, popular }(
+  //    <div className="filter-container">
 
-      <button>All</button>  
-      <button>Popular</button>
-      <button>Less Popular</button>
+  //     <button>All</button>  
+  //     <button>Popular</button>
+  //     <button>Less Popular</button>
 
-    </div>
+  //   </div>
 
-   );
+  //  );
   }
 
 
