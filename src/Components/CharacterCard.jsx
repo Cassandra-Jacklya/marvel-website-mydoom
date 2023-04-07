a85d35ff224c9f712756d2ed519cd3a92207082f
Cassandra-Jacklya <cassandrajacklya7@gmail.com>
2023-04-07 15:18:08 +0800

    updated to the latest one

--- src/Components/CharacterCard.jsx
+++ src/Components/CharacterCard.jsx
@@ -5,18 +5,16 @@ import Popup from "reactjs-popup";
 import HeroDetails from "./HeroDetails";
 
 function CharacterCard() {
-  //to store the characters
+    //to store the characters
   const [characters, setCharacters] = useState([]);
   //to store filtered characters
   const [filteredCharacters, setFilteredCharacters] = useState([]);
   //we are setting to false because it is assumed as found
   const [characterNotFound, setCharacterNotFound] = useState(false);
-  //additional state for filtering
-  const [, setFilterType] = useState("All");
 
   useEffect(() => {
     fetch(
-      "https://gateway.marvel.com/v1/public/characters?offset=0&limit=100&ts=1&apikey=066201a806fa0b522452f78b3d9c61ec&hash=9234926490e1d5b8b9276d78f8c2f00f"
+      "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=066201a806fa0b522452f78b3d9c61ec&hash=9234926490e1d5b8b9276d78f8c2f00f"
     )
       .then((response) => response.json())
       .then((data) => {
@@ -26,38 +24,13 @@ function CharacterCard() {
       .catch((error) => console.error(error));
   }, []);
 
-  //function to handle filtering
-  const handleFilterChange = (filter) => {
-    //set filterType state based on button click
-    setFilterType(filter);
-
-    switch (filter) {
-      case "All":
-        setFilteredCharacters(characters);
-        break;
-      case "Popular":
-        const popularCharacters = characters.filter(
-          (character) => character.comics.available >= 25
-        );
-        setFilteredCharacters(popularCharacters);
-        break;
-      case "Less Popular":
-        const lessPopularCharacters = characters.filter(
-          (character) => character.comics.available < 25
-        );
-        setFilteredCharacters(lessPopularCharacters);
-        break;
-      default:
-        setFilteredCharacters(characters);
-        break;
-    }
-  };
-
   const handleSearchTermChange = (searchTerm) => {
     const filtered = characters.filter((character) =>
+    //needs to be changed to lowercase so it can match correctly
       character.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setFilteredCharacters(filtered);
+    //if the length in filtered array is 0, it means that character does not exist
     setCharacterNotFound(filtered.length === 0);
   };
 
@@ -65,8 +38,8 @@ function CharacterCard() {
     <div>
       <SearchCharacter onSearchTermChange={handleSearchTermChange} />
       <div>
-        <FilterCharacter onFilterChange={handleFilterChange} />
-      </div>
+                <FilterCharacter/>
+            </div>    
       {characterNotFound ? (
         <div className="character-not-found">Character not found</div>
       ) : (
@@ -117,11 +90,21 @@ function CharacterCard() {
         //   ))}
         // </div>
       )}
-
-      <button className="load-btn">Load More</button>
     </div>
   );
 }
 
 export default CharacterCard;
+   
+   
+
+
+
+
+
+
+
+
+
+
 
