837ce40c883a8eea6a7bc4bd44ec9d4eb0b5daaf
Cassandra-Jacklya <cassandrajacklya7@gmail.com>
2023-04-10 18:40:02 +0800

    updated to latest one

--- src/Components/CharacterCard.jsx
+++ src/Components/CharacterCard.jsx
@@ -1,180 +1,10 @@
-// import React, { useState, useEffect } from "react";
-// import SearchCharacter from "../SearchCharacter";
-// import FilterCharacter from "../FilterCharacter";
-// import Popup from "reactjs-popup";
-// import HeroDetails from "./HeroDetails";
-// import HeroBookmark from "../Components/HeroBookmark";
-// import video from "../marvel.mp4";
-
-// function CharacterCard() {
-//   //to store the characters
-//   const [characters, setCharacters] = useState([]);
-//   //to store filtered characters
-//   const [filteredCharacters, setFilteredCharacters] = useState([]);
-//   //we are setting to false because it is assumed as found
-//   const [characterNotFound, setCharacterNotFound] = useState(false);
-//   //additional state for filtering
-//   const [, setFilterType] = useState("All");
-//   //setting visibility to limit character showing
-//   const [visible, setVisible] = useState(50);
-//   //to store selected character ID
-//   const [charID, setCharID] = useState({});
-//   const [loading, setLoading] = useState(true);
-//   const [filtering, setFiltering] = useState(false);
-
- 
-
-//   const loadMore = () => {
-//     setVisible(visible + 20);
-//   };
-
-  
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       let allCharacters = [];
-//       let offset = 0;
-
-//       while (offset < 100) {
-//         const response = await fetch(
-//           `https://gateway.marvel.com/v1/public/characters?offset=${offset}&limit=100&ts=1&apikey=ecb5b76db70043b36c65f8dc830aeab1&hash=65d325a029afb4ac68f2a2d5ce99ce21`
-//         );
-//         const data = await response.json();
-//         allCharacters = [...allCharacters, ...data.data.results];
-//         offset += 100;
-//       }
-
-//       setCharacters(allCharacters);
-//       setFilteredCharacters(allCharacters);
-//       setLoading(false);
-//     };
-
-//     fetchData().catch((error) => console.error(error));
-//   }, []);
-
-//   //function to handle filtering
-//   const handleFilterChange = (filter) => {
-//     //set filterType state based on button click
-//     setFilterType(filter);
-//     setFiltering(true);
-
-//     switch (filter) {
-//       case "All":
-//         setFilteredCharacters(characters);
-//         break;
-//       case "Popular":
-//         const popularCharacters = characters.filter(
-//           (character) => character.comics.available >= 25
-//         );
-//         setFilteredCharacters(popularCharacters);
-//         break;
-//       case "Less Popular":
-//         const lessPopularCharacters = characters.filter(
-//           (character) => character.comics.available < 25
-//         );
-//         setFilteredCharacters(lessPopularCharacters);
-//         break;
-//       default:
-//         setFilteredCharacters(characters);
-//         break;
-//     }
-//   };
-
-//   const handleSearchTermChange = (searchTerm) => {
-//     const filtered = characters.filter((character) =>
-//       character.name.toLowerCase().includes(searchTerm.toLowerCase())
-//     );
-//     setFilteredCharacters(filtered);
-//     setCharacterNotFound(filtered.length === 0);
-//   };
-//   // const handleCharID = () => {
-//   //   setCharID;
-//   // };
-//   return (
-//     <div>
-//       {loading ? (
-//       <div className="marvel-video">        
-//       <video autoPlay loop muted>
-//       <source src={video} type="video/mp4" />
-//       </video> 
-//       <div className="marvel-text">
-//     Sorry Marvelites, Strange is having some technical issues with his portal
-//   </div>
-//       </div>   ) : (
-        
-//       <div>        
-   
-//       <SearchCharacter onSearchTermChange={handleSearchTermChange} />
-//       <HeroBookmark />
-//       <div>
-//         <FilterCharacter onFilterChange={handleFilterChange} />
-//       </div>
-//       {characterNotFound ? (
-//         <div className="character-not-found">Character not found</div>
-//       ) : (
-//         <Popup
-//           trigger={
-//             <div className={`character-container ${filtering ? 'filtering' : ''}`}>
-//               {filteredCharacters.slice(0, visible).map((character) => (
-//                 <div
-//                   className="character-card"
-//                   key={character.id}
-//                   // created an onClick which will set the character id using setCharID(character.id)
-//                   onClick={() => setCharID(character.id)}
-//                 >
-//                   <h2 className="character-name">{character.name}</h2>
-//                   {/* potrait_uncanny is the size of pic to be shown */}
-//                   <img
-//                     className="character-card-pic"
-//                     src={`${character.thumbnail.path}/portrait_uncanny.${character.thumbnail.extension}`}
-//                     alt={character.name}
-//                   />
-//                 </div>
-//               ))}
-//             </div>
-//           }
-//           modal
-//           nested
-//         >
-//           {(close) => (
-//             <div className="modal">
-//               <button className="close" onClick={close}>
-//                 &times;
-//               </button>
-//               {/* <div className="header"> Modal Title </div> */}
-//               <div className="content">
-//                 {/* pass the charID which has the character.id using prop characterID */}
-//                 <HeroDetails characterID={charID} />
-//               </div>
-//               <div className="actions"></div>
-//             </div>
-//           )}
-//         </Popup>
-//       )}
-
-//       <div className="load-more">
-//         {visible < 100 && (
-//           <button className="load-btn " onClick={loadMore}>
-//             Load More
-//           </button>
-//         )}
-//       </div>
-//     </div>
-//      )}
-//      </div>
-     
-//   );
-// }
-// export default CharacterCard;
-
-
 import React, { useState, useEffect } from "react";
 import SearchCharacter from "../SearchCharacter";
 import FilterCharacter from "../FilterCharacter";
 import Popup from "reactjs-popup";
 import HeroDetails from "./HeroDetails";
-import HeroBookmark from "../Components/HeroBookmark";
 import video from "../marvel.mp4";
-import {AnimatePresence, motion} from "framer-motion";
+
 
 function CharacterCard() {
   //to store the characters
@@ -192,18 +22,21 @@ function CharacterCard() {
   const [loading, setLoading] = useState(true);
   const [filtering, setFiltering] = useState(false);
 
+ 
+
   const loadMore = () => {
     setVisible(visible + 20);
   };
 
+  
   useEffect(() => {
     const fetchData = async () => {
       let allCharacters = [];
       let offset = 0;
 
-      while (offset < 100) {
+      while (offset < 1000) {
         const response = await fetch(
-          `https://gateway.marvel.com/v1/public/characters?offset=${offset}&limit=100&ts=1&apikey=ecb5b76db70043b36c65f8dc830aeab1&hash=65d325a029afb4ac68f2a2d5ce99ce21`
+          `https://gateway.marvel.com/v1/public/characters?offset=${offset}&limit=100&ts=1&apikey=066201a806fa0b522452f78b3d9c61ec&hash=9234926490e1d5b8b9276d78f8c2f00f`
         );
         const data = await response.json();
         allCharacters = [...allCharacters, ...data.data.results];
@@ -240,7 +73,7 @@ function CharacterCard() {
         );
         setFilteredCharacters(lessPopularCharacters);
         break;
-         default:
+      default:
         setFilteredCharacters(characters);
         break;
     }
@@ -267,11 +100,9 @@ function CharacterCard() {
     Sorry Marvelites, Strange is having some technical issues with his portal
   </div>
       </div>   ) : (
-        
       <div>        
    
       <SearchCharacter onSearchTermChange={handleSearchTermChange} />
-      <HeroBookmark />
       <div>
         <FilterCharacter onFilterChange={handleFilterChange} />
       </div>
@@ -280,21 +111,12 @@ function CharacterCard() {
       ) : (
         <Popup
           trigger={
-            <div className={`character-container ${filtering ? 'filtering' : ''}`          
-        }>
-
-
+            <div className={`character-container ${filtering ? 'filtering' : ''}`}>
               {filteredCharacters.slice(0, visible).map((character) => (
-                <AnimatePresence>
-                <motion.div 
+                <div
                   className="character-card"
                   key={character.id}
-                  layout
-                initial ={{opacity:0}}
-                animate = {{opacity:1}}
-                transition={{duration: 0.5}}
-
-                // created an onClick which will set the character id using setCharID(character.id)
+                  // created an onClick which will set the character id using setCharID(character.id)
                   onClick={() => setCharID(character.id)}
                 >
                   <h2 className="character-name">{character.name}</h2>
@@ -304,16 +126,10 @@ function CharacterCard() {
                     src={`${character.thumbnail.path}/portrait_uncanny.${character.thumbnail.extension}`}
                     alt={character.name}
                   />
-                </motion.div>
-                </AnimatePresence>
+                </div>
               ))}
-
-
             </div>
-          }
-          modal
-          nested
-        >
+        } modal nested>
           {(close) => (
             <div className="modal">
               <button className="close" onClick={close}>
@@ -327,16 +143,18 @@ function CharacterCard() {
               <div className="actions"></div>
             </div>
           )}
+          
+          </Popup>  
+      )}
+              
 
       <div className="load-more">
-        {visible < 100 && (
-          <button className="load-btn " onClick={loadMore}>
-            Load More
-          </button>
-        )}
+        {visible < 1000 && <button className="load-btn " onClick={loadMore}>Load More</button>}
       </div>
-
     </div>
+     )}
+     </div>
+     
   );
 }
 export default CharacterCard;
